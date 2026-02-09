956c05d069b68715014d829d015948c7d623b2a6
Amit Kumar <amitraj99053@gmail.com>
2026-02-09 19:43:05 +0530

    Product Sales Analysis I

---
+++ LeetCode/Qn_1068.sql
@@ -0,0 +1,10 @@
+-- Product Sales Analysis I
+
+
+SELECT p.product_name, s.year, s.price
+FROM Sales s
+LEFT JOIN Product p
+ON s.product_id = p.product_id;
+
+
+-- https://leetcode.com/problems/product-sales-analysis-i/description/
