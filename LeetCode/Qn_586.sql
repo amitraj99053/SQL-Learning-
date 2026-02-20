1d8e3140317011036d10bfc3515a184511d0b798
Amit Kumar <amitraj99053@gmail.com>
2026-02-20 21:01:21 +0530

    Customer Placing the Largest Number of Orders

---
+++ LeetCode/Qn_586.sql
@@ -0,0 +1,9 @@
+-- Customer Placing the Largest Number of Orders
+
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(*) DESC LIMIT 1; 
+
+
+-- https://leetcode.com/problems/customer-placing-the-largest-number-of-orders/
