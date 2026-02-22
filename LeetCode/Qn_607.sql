4888db0b03c38738df5d2a6e491fff45cb02f705
Amit Kumar <amitraj99053@gmail.com>
2026-02-22 19:44:56 +0530

    Sales Person

---
+++ LeetCode/Qn_607.sql
@@ -0,0 +1,14 @@
+-- Sales Person
+
+SELECT name
+FROM SalesPerson
+WHERE sales_id NOT IN (
+    SELECT sales_id
+    FROM Orders
+    JOIN Company
+    ON Orders.com_id = Company.com_id
+    WHERE Company.name = 'RED'
+);
+
+
+-- https://leetcode.com/problems/sales-person/
