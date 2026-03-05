57c9a39277ceaf1d8a7811f5c2f08f45ca53b3aa
Amit Kumar <amitraj99053@gmail.com>
2026-03-05 10:26:27 +0530

    Department Highest Salary

---
+++ LeetCode/Qn_184.sql
@@ -0,0 +1,7 @@
+-- Department Highest Salary
+
+SELECT Department.Name AS Department, e1.Name AS Employee, e1.Salary AS Salary
+FROM Department JOIN Employee e1 ON Department.Id = e1.DepartmentId
+WHERE e1.Salary = (SELECT Max(Salary) FROM Employee e2 WHERE e2.DepartmentId = e1.DepartmentId);
+
+-- https://leetcode.com/problems/department-highest-salary/
